cb06666cc1608f2e1ddc6253cd5337b6578597af
wuzer <wuzer@yonyou.com>
2020-06-17 10:34:09 +0800

    1

--- testCli/src/components/todoList.vue
+++ testCli/src/components/todoList.vue
@@ -6,6 +6,12 @@
       <button @click="submit(value)">提交</button>
     </div>
     <todoItem :list="list" @delect="listdelect"></todoItem>
+    <div>
+      <input type="number" v-model="num" />
+      <button @click="add(num)">Add</button>
+      <button @click="rest">重置</button>
+    </div>
+    <h2>count:{{count}}</h2>
   </div>
 </template>
 <script>
@@ -20,8 +26,14 @@ export default {
     return {
       msg: "hello",
       value: "",
+      num: "",
       list: []
     };
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
   },
   methods: {
     submit: function(value) {
@@ -37,6 +49,13 @@ export default {
     },
     fatherclick: function() {
       alert("我是你爸爸");
+    },
+    add(num) {
+      this.num = "";
+      this.$store.commit("add", num);
+    },
+    rest() {
+      this.$store.state.count = 0;
     }
   }
 };
--- testCli/src/main.js
+++ testCli/src/main.js
@@ -3,13 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add(state, e) {
+      state.count = Number(state.count) + Number(e)
+    }
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
